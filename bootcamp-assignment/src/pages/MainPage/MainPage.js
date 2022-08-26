7ec2022bb48fdd3d20c658311c46ff32ee9f234e
Giga <g.uchaneishvili@gmail.com>
2022-08-27 00:48:13 +0400

    Main Page is completed with responsive

---
+++ bootcamp-assignment/src/pages/MainPage/MainPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Redberry_Title from "./Redberry_Title.svg";
+import Main from "./Main-photo-web.svg";
+import PC from "./PC.svg";
+import "./MainPage.css";
+import { Button } from "antd";
+
+function MainPage() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="redberry-title-container">
+          <img className="redberry-title" src={Redberry_Title} alt="Redberry" />
+        </div>
+        <div className="main-photo-container">
+          <img className="main-photo web" src={Main} alt="Man" />
+          <img className="main-photo mobile" src={PC} alt="PC" />
+        </div>
+        <div className="buttons-container">
+          <div className="main-button-container add-button-container">
+            <Button className="main-button add-button" type="primary">
+              ᲩᲐᲜᲐᲬᲔᲠᲘᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ
+            </Button>
+          </div>
+          <div className="main-button-container list-button-container">
+            <Button className="main-button list-button" type="primary">
+              ᲩᲐᲜᲐᲬᲔᲠᲔᲑᲘᲡ ᲡᲘᲐ
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MainPage;
